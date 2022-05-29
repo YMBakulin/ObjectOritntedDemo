79469243e523a2f8244ff5b587010319cce3da7b
YMBakulin <106443179+YMBakulin@users.noreply.github.com>
2022-05-29 23:40:20 +0300

    Initial commit

---
+++ src/com/ymbakulin/Main.java
@@ -0,0 +1,30 @@
+package com.ymbakulin;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner inputHourlyRate = new Scanner(System.in);
+        System.out.print("Please enter hourly rate for Peter: " );
+        int myHourlyRate = 0;
+       do {
+           myHourlyRate = inputHourlyRate.nextInt();
+           if (myHourlyRate<=0) System.out.println("Error: hourly rate cannot be smaller then zero!");
+       } while (myHourlyRate <= 0);
+
+Staff staff1 = new Staff("Peter", myHourlyRate);
+staff1.setHoursWorked(160);
+int pay = staff1.calculatePay(1000, 400);
+System.out.println("Pay = " + pay);
+
+Staff staff2 = new Staff("Kiwako", "Ito", 35);
+staff2.setHoursWorked(160);
+System.out.println("\nHours Worked = " + staff2.getHoursWorked());
+pay = staff2.calculatePay();
+System.out.println("Pay = " + pay);
+
+
+    }
+}
---
+++ src/com/ymbakulin/Staff.java
@@ -0,0 +1,59 @@
+package com.ymbakulin;
+
+public class Staff {
+    private String nameOfStaff;
+    private int hourlyRate;
+    private int hoursWorked;
+
+    public Staff(String name, int hRate)
+    {
+        nameOfStaff = name;
+        hourlyRate = hRate;
+        System.out.println("\n" + nameOfStaff);
+        System.out.println("---------------------------------");
+    }
+
+    public Staff(String firstName, String lastName, int hRate)
+    {
+        nameOfStaff = firstName + " " + lastName;
+        hourlyRate = hRate;
+        System.out.println("\n" + nameOfStaff);
+        System.out.println("---------------------------------");
+    }
+
+    public void printMessage()
+    {
+        System.out.println("Calculating Pay...");
+    }
+
+    public int calculatePay()
+    {
+        printMessage();
+
+        int staffPay;
+        staffPay = hoursWorked * hourlyRate;
+        if (hoursWorked>0) return staffPay;
+        else return -1;
+    }
+
+    public int calculatePay(int bonus, int allowance)
+    {
+        printMessage();
+        if (hoursWorked>0) return hoursWorked*hourlyRate + bonus + allowance;
+        else return 0;
+    }
+
+    public void setHoursWorked(int hours)
+    {
+        if (hours>0) hoursWorked = hours;
+        else {
+            System.out.println("Error: HoursWorked cannot be smaller then zero");
+            System.out.println("Error: HoursWorked not updated");
+        }
+    }
+
+    public int getHoursWorked()
+    {
+        return hoursWorked;
+    }
+}
