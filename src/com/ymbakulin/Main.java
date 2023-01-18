93dc3d61226f8024cd178e715b692bed451dc3a3
YMBakulin <yuriy_662@rambler.ru>
2023-01-18 23:25:53 +0300

    Update Main.java

--- src/com/ymbakulin/Main.java
+++ src/com/ymbakulin/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	// write your code here
         // Set new status to my github project
         Scanner inputHourlyRate = new Scanner(System.in);
-        System.out.print("Please enter hourly rate for Mr. yuriy: " );
+        System.out.print("Please enter hourly rate for Mr. Yuriy Bakulin: " );
         int myHourlyRate = 25;
        do {
            myHourlyRate = inputHourlyRate.nextInt();
