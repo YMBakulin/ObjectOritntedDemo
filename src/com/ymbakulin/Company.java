466c8ef063fa9095caecccaebc66306b74c7a2a0
YMBakulin <106443179+YMBakulin@users.noreply.github.com>
2023-10-03 22:56:40 +0300

    Add getters and setters

--- src/com/ymbakulin/Company.java
+++ src/com/ymbakulin/Company.java
@@ -7,4 +7,35 @@ public class Company {
     String address;
     Long phoneNumber = 23L;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
